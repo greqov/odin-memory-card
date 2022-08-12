8fa1c47e77ac0255718e4cb9cec21b8fbc369603
Ivan Greqov <idgreq@bk.ru>
2022-08-12 11:44:48 +0300

    Add page layout (header, footer, main)

--- src/js/App.js
+++ src/js/App.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Main from './components/Main';
 
 export default function App() {
-  return <div>Such App</div>;
+  return (
+    <div className="relative min-h-screen flex flex-col font-sans bg-zinc-50 selection:bg-pink-300">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
 }
---
+++ src/js/components/Main.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Main() {
+  return <main className="container max-w-4xl mx-auto mb-6 px-4">unoos</main>;
+}
